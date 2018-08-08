f870c94101a51aefee6ee1d0f98274f10399cdb5
GIANPIERO-PC\GIANPIERO <gianpieroguar@gmail.com>
2018-08-08 14:10:15 +0200

    Aggiunta anamnesi

--- src/views/AnamnesiController.java
+++ src/views/AnamnesiController.java
@@ -12,6 +12,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,6 +21,40 @@ import rinocitologia.*;
 import utility.*;
 
 public class AnamnesiController implements Initializable {
+
+    @FXML // fx:id="textAnamFam"
+    private TextArea textAnamFam; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textAnamFis"
+    private TextArea textAnamFis; // Value injected by FXMLLoader
+
+    @FXML // fx:id="aggiungiAnam"
+    private Button aggiungiAnam; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textAnamPatRemota"
+    private TextArea textAnamPatRemota; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textAnamPatProssima"
+    private TextArea textAnamPatProssima; // Value injected by FXMLLoader
+
+    //Variabili per tenere traccia delle informazioni delle varie anamnesi
+    private String tipoParto="";
+    private String alcool="";
+    private String alimentazione="";
+    private String attivita="";
+    private String caffeina="";
+    private String esecuzioneParto="";
+    private String fumatore="";
+    private String riposo="";
+    private String scuola="";
+    private String sviluppo="";
+    private String droga="";
+    private String stress="";
+    private String eta="";
+    private String allergia="";
+    private String anamFamiliare="";
+    private String anamProssima="";
+    private String anamRemota="";
 
     private Patient patient;
 
@@ -141,4 +177,66 @@ public class AnamnesiController implements Initializable {
         Utility util = new Utility(patient);
         util.writeLastSession();
      }
+
+    @FXML
+    void addAnam(ActionEvent event) throws IOException {
+        //Main.showQuestions("Anamnesi familiare");
+        Parent quest = FXMLLoader.load(getClass().getResource("../views/Questions.fxml"));
+        Scene quest_page = new Scene(quest);
+        Stage quest_stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+        quest_stage.setScene(quest_page);
+        QuestionsController.setInfo();
+        quest_stage.show();
+    }
+
+    public void getInfo(String tipoParto, String alcool, String alimentazione, String attivita, String caffeina, String esecuzioneParto, String fumatore, String riposo, String scuola, String sviluppo, String droga, String stress, String eta, String allergia, String anamFamiliare, String anamProssima, String anamRemota){
+        this.tipoParto=tipoParto;
+        this.alcool=alcool;
+        this.alimentazione=alimentazione;
+        this.attivita=attivita;
+        this.caffeina=caffeina;
+        this.esecuzioneParto=esecuzioneParto;
+        this.fumatore=fumatore;
+        this.riposo=riposo;
+        this.scuola=scuola;
+        this.sviluppo=sviluppo;
+        this.droga=droga;
+        this.stress=stress;
+        this.eta=eta;
+        this.allergia=allergia;
+        this.anamFamiliare=anamFamiliare;
+        this.anamProssima=anamProssima;
+        this.anamRemota=anamRemota;
+        System.out.println(this.tipoParto);
+        System.out.println(this.anamFamiliare);
+    }
 }
+
+
+
+
+
+
+/*
+        domandeAnamFisiologica.add("Che tipo di parto ha avuto? (pretermine, termine, post-termine)");
+        domandeAnamFisiologica.add("Che tipo di parto ha avuto? (naturale eutocico, naturale distocito, operatorio cesareo)");
+        domandeAnamFisiologica.add("Che tipo di allattamento ha avuto?(materno, artificiale, mercenario)");
+        domandeAnamFisiologica.add("Che tipo di sviluppo vegetativo e relazionale ha avuto?(Normale, patologico)");
+        domandeAnamFisiologica.add("A quale livello scolastico fa riferimento?(analfabeta, elementare, medie-inferiori, diploma, laurea)");
+        domandeAnamFisiologica.add("A quale livello di attività fisica fa riferimento?(bassa, alta)");
+        domandeAnamFisiologica.add("Quali sono le sue abitudini alimentari?(sano, non sano)");
+        domandeAnamFisiologica.add("Qual'è il ritmo sonno/sveglia?(ideale, non ideale)");
+        domandeAnamFisiologica.add("Quanto stress fisico/psicologico ha accumulato?(poco, molto)");
+        domandeAnamFisiologica.add("Fuma? Se si da quanto tempo?(si, no)");
+        domandeAnamFisiologica.add("Beve molti alcolici?(si, no)");
+        domandeAnamFisiologica.add("Fa uso di droghe?(si, no)");
+        domandeAnamFisiologica.add("Assume molta caffeina?");
+        domandeAnamFisiologica.add("Quante gravidanze ha avuto?");
+        //Chiedere l'età in cui si è avuto, l'inizio e la fine della gravidanza, l'esito, il sesso del bambino
+        domandeAnamFisiologica.add("Qual'è l'età menarca? (della prima mestruazione)(un numero da 8 a 18)");
+        domandeAnamFisiologica.add("Il ciclo mestruale è regolare?(si, no)");
+        domandeAnamFisiologica.add("La menopaura è fisiologica?(si, no)");
+        domandeAnamFisiologica.add("Che tipo di professione svolge?");
+        domandeAnamFisiologica.add("Qual'è la condizione dell'alvo?(regolare, stitico, diarroico, alterno)");
+        domandeAnamFisiologica.add("Qual'è la condizione della minzione?(regolare, patologica)");
+*/
---
+++ src/views/QuestionsController.java
@@ -0,0 +1,244 @@
+package views;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import rinocitologia.Patient;
+import utility.Utility;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class QuestionsController  implements Initializable {
+
+    @FXML
+    private TextArea textFam;
+
+    @FXML
+    private TextArea textPross;
+
+    @FXML
+    private TextArea textRem;
+
+    @FXML
+    private ComboBox<String> comboBoxTipoParto;
+
+    @FXML
+    private ComboBox<String> comboBoxEsecuzioneParto;
+
+    @FXML
+    private ComboBox<String> comboBoxSviluppo;
+
+    @FXML
+    private ComboBox<String> comboBoxScuola;
+
+    @FXML
+    private ComboBox<String> comboBoxAttivita;
+
+    @FXML
+    private ComboBox<String> comboBoxAlimentazione;
+
+    @FXML
+    private ComboBox<String> comboBoxRiposo;
+
+    @FXML
+    private ComboBox<String> comboBoxStress;
+
+    @FXML
+    private ComboBox<String> comboBoxDroga;
+
+    @FXML
+    private ComboBox<String> comboBoxFumatore;
+
+    @FXML
+    private ComboBox<String> comboBoxAlcolici;
+
+    @FXML
+    private ComboBox<String> comboBoxCaffeina;
+
+    @FXML
+    private TextField textFieldAllergia;
+
+    @FXML
+    private TextField textFieldEta;
+
+    private AnamnesiController anamnesiController;
+
+    private Patient patient;
+
+    public void setPatient(Patient patient) {this.patient = patient;}
+
+
+    /*
+     *
+     * START SIDEBAR COMMANDS
+     *
+     */
+
+    @FXML
+    private void anamnesiCaller(ActionEvent event)  throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getResource("Anamnesi.fxml"));
+        try {
+            Loader.load();
+        } catch (IOException ex){
+            Logger.getLogger(AnamnesiController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+        AnamnesiController controller = Loader.getController();
+        controller.setPatient(patient);
+
+        //Inizio Carica View
+        Parent p = Loader.getRoot();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(p));
+        stage.show();
+    }
+
+    @FXML
+    private void insertCells(ActionEvent event)  throws IOException{
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getResource("Home.fxml"));
+        try {
+            Loader.load();
+        } catch (IOException ex){
+            Logger.getLogger(AnamnesiController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+        HomeController controller = Loader.getController();
+        controller.setPatient(patient);
+
+
+        //Inizio Carica View
+        Parent p = Loader.getRoot();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(p));
+        stage.show();
+    }
+
+
+    @FXML
+    private void revisionCaller(ActionEvent event)  throws IOException{
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getResource("Revisione.fxml"));
+        try {
+            Loader.load();
+        } catch (IOException ex){
+            Logger.getLogger(AnamnesiController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+        RevisioneController controller = Loader.getController();
+        controller.setPatient(patient);
+
+        //Inizio Carica View
+        Parent p = Loader.getRoot();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(p));
+        stage.show();
+    }
+
+
+    @FXML
+    private void diagnosisCaller(ActionEvent event)  throws IOException{
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getResource("Diagnosi.fxml"));
+        try {
+            Loader.load();
+        } catch (IOException ex){
+            Logger.getLogger(AnamnesiController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+        DiagnosiController controller = Loader.getController();
+        controller.setPatient(patient);
+
+        //Inizio Carica View
+        Parent p = Loader.getRoot();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(p));
+        stage.show();
+    }
+
+    @FXML
+    private void anagraficaCaller(ActionEvent event)  throws IOException{
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getResource("Anagrafica.fxml"));
+        try {
+            Loader.load();
+        } catch (IOException ex){
+            Logger.getLogger(AnamnesiController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+        AnagraficaController controller = Loader.getController();
+        controller.setPatient(patient);
+
+        //Inizio Carica View
+        Parent p = Loader.getRoot();
+        if (patient.getFirstName() != "Anonimo"){
+            controller.setFields();
+        }
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(p));
+        stage.show();
+    }
+
+    public void shutdown() {
+        // cleanup code here...
+        System.out.println("SAVING SESSION");
+        Utility util = new Utility(patient);
+        util.writeLastSession();
+    }
+
+    public static void setInfo(){
+
+    }
+
+    public void setAnamnesiController(AnamnesiController anam){
+        this.anamnesiController = anam;
+    }
+
+    @FXML
+    void salva(ActionEvent event) {
+        if(textFieldEta.getText().isEmpty())
+            textFieldEta.setText("");
+        anamnesiController.getInfo(comboBoxTipoParto.getSelectionModel().getSelectedItem(),
+                comboBoxAlcolici.getSelectionModel().getSelectedItem(),
+                comboBoxAlimentazione.getSelectionModel().getSelectedItem(),
+                comboBoxAttivita.getSelectionModel().getSelectedItem(),
+                comboBoxCaffeina.getSelectionModel().getSelectedItem(),
+                comboBoxEsecuzioneParto.getSelectionModel().getSelectedItem(),
+                comboBoxFumatore.getSelectionModel().getSelectedItem(),
+                comboBoxRiposo.getSelectionModel().getSelectedItem(),
+                comboBoxScuola.getSelectionModel().getSelectedItem(),
+                comboBoxSviluppo.getSelectionModel().getSelectedItem(),
+                comboBoxDroga.getSelectionModel().getSelectedItem(),
+                comboBoxStress.getSelectionModel().getSelectedItem(),
+                textFieldEta.getText(),
+                textFieldAllergia.getText(),
+                textFam.getText(),
+                textPross.getText(),
+                textRem.getText()
+                );
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        comboBoxTipoParto.setItems(FXCollections.observableArrayList("pretermine", "termine", "post-termine"));
+        comboBoxAlcolici.setItems(FXCollections.observableArrayList("nessuno", "irrilevante", "uso eccessivo"));
+        comboBoxAlimentazione.setItems(FXCollections.observableArrayList("sana", "errata"));
+        comboBoxAttivita.setItems(FXCollections.observableArrayList("assente", "poca attività", "molta attività"));
+        comboBoxCaffeina.setItems(FXCollections.observableArrayList("nessuno", "irrilevante", "uso eccessivo"));
+        comboBoxEsecuzioneParto.setItems(FXCollections.observableArrayList("naturale eutocico", "naturale distocito", "operatorio cesareo"));
+        comboBoxFumatore.setItems(FXCollections.observableArrayList("si", "no"));
+        comboBoxRiposo.setItems(FXCollections.observableArrayList("ideale", "non ideale"));
+        comboBoxScuola.setItems(FXCollections.observableArrayList("analfabeta", "elementare", "medie-inferiori", "diploma", "laurea"));
+        comboBoxSviluppo.setItems(FXCollections.observableArrayList("normale", "patologico"));
+        comboBoxDroga.setItems(FXCollections.observableArrayList("si", "no"));
+        comboBoxStress.setItems(FXCollections.observableArrayList("poco", "molto"));
+        setInfo();
+    }
+}
